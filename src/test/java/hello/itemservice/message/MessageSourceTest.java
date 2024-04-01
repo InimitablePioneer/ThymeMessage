3e412e6d168538502fe0f3467e2de7d83cf19330
InimitablePioneer <seungwonpioneer@gmail.com>
2024-04-01 23:19:19 +0900

    Apr1 at home via MAC

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,5 +1,24 @@
 package hello.itemservice.message;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    public void helloMessage() {
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
 
 }
