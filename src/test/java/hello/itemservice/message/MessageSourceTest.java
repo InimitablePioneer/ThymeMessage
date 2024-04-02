4916230f4b6fddb4aee9e5fd3b73be6e0db141a3
InimitablePioneer <seungwonpioneer@gmail.com>
2024-04-02 16:39:36 +0900

    Apr2 at work via WIN

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,6 +19,26 @@ public class MessageSourceTest {
     public void helloMessage() {
         String result = ms.getMessage("hello", null, null);
         assertThat(result).isEqualTo("안녕");
+        System.out.println(result);
+    }
+    @Test
+    public void notFoundMessageCode() {
+        ms.getMessage("no-code", null, null);
+        assertThatThrownBy(() -> ms.getMessage("no-code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+    @Test
+    public void notFoundMessafeCodeDefaultMessage() {
+        String result = ms.getMessage("no-code", null, "기본 메세지", null);
+        System.out.println(result);
+        assertThat(result).isEqualTo("기본 메세지");
+    }
+
+    @Test
+    public void argumentMessage() {
+        String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(result).isEqualTo("안녕 Spring");
+        System.out.println(result);
     }
 
 
